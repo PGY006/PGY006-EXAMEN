23c5454e955f857442b1476bc2c1c7c2963b494e
Lucas Andress Rojas Pailahueque <pgy006.lbe@gmail.com>
2022-04-11 18:03:10 -0400

    Arreglos de HREF

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -17,27 +17,78 @@ function cerrarsesion(){
       })
   }
 
-function alerta(){
+    function alerta(){
 
-    Swal.fire({
-        title: '¿Esta seguro de continuar con la compra?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#42E51A',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Sí, confirmo!',
-        cancelButtonText: 'Cancelar'
-        }).then((result) => {
-        if (result.isConfirmed) {
-            Swal.fire(
-            'Gracias!',
-            'Tu pedido se ha confirmado.',
-            'success'
-            )
+        Swal.fire({
+            title: '¿Esta seguro de continuar con la compra?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#42E51A',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, confirmo!',
+            cancelButtonText: 'Cancelar',
+            }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Gracias! Tu pedido se ha confirmado',
+                    showConfirmButton: false
+                })
+                setTimeout(myURL,1500);
+                function myURL(){
+                    location.href ="indexRegistrado.html"
+                }
+            }
+            })
         }
-        })
-    }
+        function alertaNR(){
 
+            Swal.fire({
+                title: '¿Esta seguro de continuar con la compra?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#42E51A',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí, confirmo!',
+                cancelButtonText: 'Cancelar',
+                }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Gracias! Tu pedido se ha confirmado',
+                        showConfirmButton: false
+                    })
+                    setTimeout(myURL,1500);
+                    function myURL(){
+                        location.href ="index.html"
+                    }
+                }
+                })
+            }
+    function alertaSuscrito(){
+
+        Swal.fire({
+            title: '¿Esta seguro de continuar con la compra?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#42E51A',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, confirmo!',
+            cancelButtonText: 'Cancelar',
+            }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Gracias! Tu pedido se ha confirmado',
+                    showConfirmButton: false
+                  })
+                setTimeout(myURL,1500);
+                function myURL(){
+                    location.href ="indexSuscrito.html"
+                }
+            }
+            })
+        }
 function registrado(){
     Swal.fire({
         icon: 'success',
@@ -68,7 +119,7 @@ function registrado(){
             Swal.fire({
                 icon: 'success',
                 title: 'Te has suscrito con exito',
-                showConfirmButton: false,
+                showConfirmButton: false
               })
 
             setTimeout(myURL,1500);
